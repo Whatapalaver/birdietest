9523eec03de4d1dd1c9c033cd5cfc2aa07111add
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-16 13:45:04 +0100

    Setting up db connection with test query in databasePool.js

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const app = express();
 const port = 3306;
 
+// move this to the api folder when you have a better idea of api structure
+// replace with an app.use which will direct to the new api router
 app.get('/api', (req, res) => {
   res.json({});
 });
 
-module.exports = app;""
+module.exports = app;
---
+++ backend/databasePool.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const databaseConfiguration = require('./secrets/databaseConfiguration');
+const connection = mysql.createConnection(databaseConfiguration);
+
+connection.connect()
+
+connection.query('SELECT COUNT(*) FROM census_learn_sql;', function (err, response) {
+  if (err) throw err
+
+  console.log('Row count is: ', response);
+})
+
+connection.end()
+
+module.exports = connection;
