8f3b1361ad031b30cba966e734aa43d2239d845e
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-19 12:12:24 +0100

    Linking FieldSelector component to fields api

--- backend/app/index.js
+++ backend/app/index.js
@@ -3,11 +3,13 @@ const app = express();
 const dataAPI = require('../api/data');
 const fieldAPI = require('../api/fields');
 const connection = require('../databasePool');
+let cors = require('cors');
 
 // move this to the api folder when you have a better idea of api structure
 // replace with an app.use which will direct to the new api router
 
 connection.connect();
+app.use(cors())
 app.use('/api/data', dataAPI)
 app.use('/api/fields', fieldAPI)
 
