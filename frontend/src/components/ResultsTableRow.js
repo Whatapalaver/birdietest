e38d5885a0fee76fe6defd84584613930863ad47
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-22 15:13:56 +0100

    Adding a loading indicator to table rows component

--- frontend/src/components/ResultsTableRow.js
+++ frontend/src/components/ResultsTableRow.js
@@ -11,8 +11,18 @@ class ResultsTableRow extends React.Component {
       <td className='pv2 ph3 tr' key={data.avAge + data.name} value={data.avAge}>{data.avAge}</td>
     </tr>
     );
+    const loading =
+    <tr className='striped--light-gray tl' key="loading">
+      <td className='pv2 ph3' key="loading0" value="loading">Loading.....</td>
+      <td className='pv2 ph3 tr' key="loading000" value="0">0</td>
+      <td className='pv2 ph3 tr' key="loading001" value="0">0</td>
+    </tr>
 
-    return (row);
+    if (data.length === 0) {
+      return (loading); 
+    } else {
+      return (row);
+    }
   }
 }
 
