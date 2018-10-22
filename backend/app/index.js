bb6519570577ae1be152e7b4ff0d69870b281618
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-22 10:59:46 +0100

    Requiring path

--- backend/app/index.js
+++ backend/app/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dataAPI = require('../api/data');
 const fieldAPI = require('../api/fields');
 const connection = require('../databasePool');
+const path = require('path')
 let cors = require('cors');
 
 // move this to the api folder when you have a better idea of api structure
